a585ab5ada9d14d4404739889bab3fe552be743c
Justin Revenaugh <justinr@umn.edu>
2018-05-22 12:23:13 -0500

    Added instruction text and basic UI items.

--- server.R
+++ server.R
@@ -17,20 +17,16 @@ server <- function(input, output, session) {
   observeEvent(input$help, {
     showModal(modalDialog(
       title = "Instructions",
-      HTML(paste("Guide the red car around the track in as few moves as possible.",
-                 "Your car's momentum dictates where you can go.  Click any",
-                 "green circle to move.  Be sure to slow down before turns and avoid",
-                 "the barricades (red circles).  If you crash, get back on the track",
-                 "as quickly as possible and your car will (slowly) regain full throttle.",
-                 tags$br(), tags$br(),
-                 "You can undo as many moves as needed if you get in trouble,",
-                 "but beware--the blue car keeps going.",
+      HTML(paste("Players take turns drawing vertical or horizontal lines between dots.",
+                 "The player who draws the fourth side of a small squares scores one point",
+                 "and goes again.",
+                 "When all lines are drawn, the player with the most completed squares wins.",
                  tags$br(), tags$br(),
                  "Justin Revenaugh", tags$br(),
                  "Earth Sciences", tags$br(),
                  "University of Minnesota", tags$br(),
                  "justinr@umn.edu", tags$br(),
-                 "Code at: github.com/jrevenaugh/RacerX"
+                 "Code at: github.com/jrevenaugh/BoxR"
                 )
       ),
       easyClose = TRUE)
--- ui.R
+++ ui.R
@@ -5,21 +5,37 @@
 
 require(shiny)
 
-ui <- bootstrapPage(
+ui <- bootstrapPage(title = "BoxR v1.0 - Dots and Boxes",
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   plotOutput(outputId = "gamegrid",
              height = "100%",
              width = "100%",
              click = "click"),
 
-  absolutePanel(top = 10, left = 10, width = "260px", draggable = FALSE,
-                wellPanel(h4("BoxR V1.0"),
-                          actionButton(inputId = "undo",
-                                       label = "Undo",
-                                       width = "100%",
-                                       style = "margin-bottom: 5px;"),
+  absolutePanel(top = 10, left = 10, width = "260px", draggable = TRUE,
+                wellPanel(h4("Grid Dimensions"),
+                          fluidRow(
+                            column(5,
+                              numericInput(inputId = "grows",
+                                           label = "Rows",
+                                           min = 3, max = 10, step = 1,
+                                           value = 4)
+                            ),
+                            column(2),
+                            column(5,
+                              numericInput(inputId = "gcols",
+                                           label = "Columns",
+                                           min = 3, max = 10, step = 1,
+                                           value = 4)
+                            )
+                          ),
+                          radioButtons(inputId = "nPlayers",
+                                       label = "Players",
+                                       choices = c("One (play the computer)" = 1,
+                                                   "Two (play a friend)" = 2),
+                                       selected = 1),
                           actionButton(inputId = "reset",
-                                       label = "New Race",
+                                       label = "New Game",
                                        width = "100%",
                                        style = "margin-bottom: 5px;"),
                           actionButton(inputId = "help",
